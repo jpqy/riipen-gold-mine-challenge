dbd19a7d46fcb09a1c22eb9b4ed0a64e79609089
Jimmy Peng <jimmy@jpeng.ca>
2020-09-26 13:37:06 -0400

    Add failing test for the sum

--- src/helpers/calculateNextColumn.test.js
+++ src/helpers/calculateNextColumn.test.js
@@ -12,5 +12,6 @@ describe("Calculating the next column", () => {
   it("returns an array", async () => {
     const result = calculateNextColumn(FIRST_COLUMN, SECOND_COLUMN);
     expect(Array.isArray(result)).toBe(true);
+    expect(result[0].sum).toBe(7); // The 5 in the second column should add the 2 from the first column
   });
 });
