0acb942f350851009add18be7d983bba7190512e
Jimmy Peng <jimmy@jpeng.ca>
2020-09-26 13:56:52 -0400

    Add test for correct direction flags being set
    The return value of calculateNextColumn should include a direction flag,
    specifying what direction(s) were taken to achieve the newly calculated
    sum. This flag is used to determine the legality of subsequent moves.

--- src/helpers/calculateNextColumn.test.js
+++ src/helpers/calculateNextColumn.test.js
@@ -20,4 +20,10 @@ describe("Calculating the next column", () => {
     expect(result[1].sum).toBe(9); // The 6 in the second column should add the 3 from the first column
     expect(result[2].sum).toBe(10); // The 7 in the second column should add the 3 from the first column
   });
+
+  it("calculates the direction flag of the next column correctly", async () => {
+    expect(result[0].dirFlag).toBe("u"); // The move from 2 to 5 is an upwards move
+    expect(result[1].dirFlag).toBe("u"); // The move from 3 to 6 is upwards
+    expect(result[2].dirFlag).toBe("r"); // The move from 3 to 7 is rightwards
+  });
 });
